6c149c1ed80ada4cac89bc645c3018c5bcd7a3da
ASAA19971a <abel.2833@hotmail.com>
2020-10-22 18:06:46 -0500

    a

---
+++ js/calculadora.js
@@ -0,0 +1,21 @@
+function insertar(num) {
+    document.formulario.pantalla.value = document.formulario.pantalla.value + num;
+
+}
+
+function resultado() {
+    var datos = document.formulario.pantalla.value;
+
+    if (datos) {
+        document.formulario.pantalla.value = eval(datos);
+    }
+}
+
+function limpiar() {
+    document.formulario.pantalla.value = "";
+}
+
+function borrar() {
+    var datos = document.formulario.pantalla.value;
+    document.formulario.pantalla.value = datos.substring(0, (datos.length - 1));
+}
