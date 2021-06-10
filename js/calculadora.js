f6480e1928e59bf905ad250608303eaea0a26181
ASAA19971a <abel.2833@hotmail.com>
2021-06-09 19:38:42 -0500

    Estilos y funcion solo numeros

--- js/calculadora.js
+++ js/calculadora.js
@@ -19,3 +19,8 @@ function borrar() {
     var datos = document.formulario.pantalla.value;
     document.formulario.pantalla.value = datos.substring(0, (datos.length - 1));
 }
+
+function soloNumeros(e) {
+    var key = window.Event ? e.which : e.keyCode
+    return (key >= 42 && key <= 57 && key != 44) //0 - 9 y + - * / .
+}
